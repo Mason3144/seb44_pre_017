fc03f7d313accc45fcb6b76994719dfe4e0863cd
Isu Shin <111070569+isu-nice@users.noreply.github.com>
2023-06-15 15:38:47 +0900

    Initial commit

---
+++ server/src/main/java/synergy_overflow/SynergyOverflowApplication.java
@@ -0,0 +1,13 @@
+package synergy_overflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyOverflowApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/synergy_overflow/SynergyOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package synergy_overflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
