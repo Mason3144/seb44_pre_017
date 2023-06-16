a641e85e38a22a27ccf735b607d3c3f8e5b909ae
Mason3144 <lts890303@gmail.com>
2023-06-16 15:48:16 +0900

    [test] 예외처리 테스트 완료 (#54)

--- server/src/main/java/synergy_overflow/question/controller/QuestionController.java
+++ server/src/main/java/synergy_overflow/question/controller/QuestionController.java
@@ -5,22 +5,24 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import synergy_overflow.question.dto.QuestionDto;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/questions")
 @Validated
 public class QuestionController {
-    @PatchMapping("{question-id}")
-    public ResponseEntity patchQuestion(@RequestBody QuestionDto.Request request,
-                                        @PathVariable("question-id") @Positive long questionId,
-                                        @RequestParam @Positive int filtered){
-
-        System.out.println(request.getTitle());
-        System.out.println(questionId);
-        System.out.println(filtered);
-
-
-        return null;
-    }
+//      예외처리 테스트용입니다. 실제 로직 작성시 지워주세요
+//    @PatchMapping("{question-id}")
+//    public ResponseEntity patchQuestion(@RequestBody @Valid QuestionDto.Request request,
+//                                        @PathVariable("question-id") @Positive long questionId,
+//                                        @RequestParam @Positive int filtered){
+//
+//        System.out.println(request.getTitle());
+//        System.out.println(questionId);
+//        System.out.println(filtered);
+//
+//
+//        return null;
+//    }
 }
--- server/src/main/java/synergy_overflow/question/dto/QuestionDto.java
+++ server/src/main/java/synergy_overflow/question/dto/QuestionDto.java
@@ -4,15 +4,18 @@ import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
 public class QuestionDto {
 
 
-    // test용 입니다.
-    @Builder
-    @Getter
-    public static class Request{
-        @NotBlank
-        private String title;
-    }
+    // 예외 컨트롤 test용 입니다. 실제 로직 작성시 지워주세요
+//    @Builder
+//    @Getter
+//    public static class Request{
+//        @Positive
+//        private long questionId;
+//        @NotBlank
+//        private String title;
+//    }
 }
