9d0e0a5fa5cbc10e4cae3570d6db05be5026533d
Mason3144 <lts890303@gmail.com>
2023-06-16 15:45:34 +0900

    [feat] 예외처리 로직 완료 (#54)

--- server/src/main/java/synergy_overflow/exception/advice/GlobalExceptionAdvice.java
+++ server/src/main/java/synergy_overflow/exception/advice/GlobalExceptionAdvice.java
@@ -6,20 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import synergy_overflow.exception.businessLogicException.BusinessLogicException;
 import synergy_overflow.exception.errorResponse.ErrorResponse;
 
-import javax.validation.UnexpectedTypeException;
-import java.util.Arrays;
+import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
+    // req body의 validation 불일치 ex) @NotBlank
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBindException(
@@ -29,17 +29,30 @@ public class GlobalExceptionAdvice {
         return response;
     }
 
+    //query parameter의 validation 불일치 ex) @Positive
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleUnexpectedTypeException(
-            UnexpectedTypeException e) {
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e){
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+        return response;
+    }
 
-        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
-                "Required request body is missing");
+    //쿼리 파라미터 타입 불량
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+        String message = "Query Parameter, '" +
+                e.getParameter().getParameter().getName() +
+                "'의 타입은 '" +
+                e.getParameter().getParameter().getType()+
+                "' 이어야 됩니다.";
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,message);
 
         return response;
     }
 
+    // 비지니스로직 사용자 정의 예외
     @ExceptionHandler
     public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
         final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
@@ -48,6 +61,7 @@ public class GlobalExceptionAdvice {
                 .getStatus()));
     }
 
+    // 특정 URI에 맞지 않는 HTTP 메서드 요청이 들어올경우
     @ExceptionHandler
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ErrorResponse handleHttpRequestMethodNotSupportedException(
@@ -58,17 +72,18 @@ public class GlobalExceptionAdvice {
         return response;
     }
 
+    //DTO의 필드가 1개 일경우 MVC에서 직렬화 작업이 잘 이루어지지 않음
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleHttpMessageNotReadableException(
             HttpMessageNotReadableException e) {
-
         final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
-                "Required request body is missing");
+                "DTO의 필드가 2개 이상이어야 합니다. 서버 관리자에게 문의하세요");
 
         return response;
     }
 
+    //서블릿 관련 예외
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMissingServletRequestParameterException(
@@ -80,11 +95,11 @@ public class GlobalExceptionAdvice {
         return response;
     }
 
+    // 핸들 되지 않는 예외
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception e) {
         log.error("# handle Exception", e);
-        // 관리자 이메일로 로그 보내기
 
         final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
         return response;
