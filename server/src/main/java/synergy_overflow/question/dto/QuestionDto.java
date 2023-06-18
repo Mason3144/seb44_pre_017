4cb336a003b016398159d3cc0e4071861a72e433
Mason3144 <lts890303@gmail.com>
2023-06-18 23:14:55 +0900

    [feat] Question Entity와 Dto 생성 (no space) (#5, #7, #,8 #16, #63)

--- server/src/main/java/synergy_overflow/question/dto/QuestionDto.java
+++ server/src/main/java/synergy_overflow/question/dto/QuestionDto.java
@@ -2,10 +2,45 @@ package synergy_overflow.question.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import synergy_overflow.helper.validator.NotSpace;
+import synergy_overflow.question.temporaries.temporaryDtos.AnswerDto;
+import synergy_overflow.question.temporaries.temporaryDtos.WriterDto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class QuestionDto {
+    @Builder
+    @Getter
+    public static class Post{
+        @NotBlank
+        @Size(min = 0, max = 50)
+        private String title;
+        @NotBlank
+        @Size(min = 20, max = 255)
+        private String body;
+    }
+    @Builder
+    @Getter
+    public static class Patch{
+        @NotSpace
+        @Size(min = 0, max = 50)
+        private String title;
+        @NotSpace
+        @Size(min = 20, max = 255)
+        private String body;
 
+    }
+    @Builder
+    @Getter
+    public static class Response{
+        private long question_id;
+        private String title;
+        private String body;
+        private LocalDateTime created_at;
+        private WriterDto.Response writer;
+        private List<AnswerDto.Response> answers;
+    }
 }
