eacb33a549db83831f9644c458975741390ce009
isu-nice <isu010614@naver.com>
2023-06-18 12:25:16 +0900

    [feat] claim 반환 기능 구현 (#13)

--- server/src/main/java/synergy_overflow/auth/jwt/JwtTokenizer.java
+++ server/src/main/java/synergy_overflow/auth/jwt/JwtTokenizer.java
@@ -1,9 +1,14 @@
-package synergy_overflow.auth;
+package synergy_overflow.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -11,9 +16,21 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtTokenizer {
+    @Getter
+    @Value("${jwt.key}")
+    private String secretKey;
 
-    public String encodedBase64SecretKey(String secretKey) {
+    @Getter
+    @Value("${jwt.access-token-expiration-minutes}")
+    private int accessTokenExpirationMinutes;
+
+    @Getter
+    @Value("${jwt.refresh-token-expiration-minutes}")
+    private int refreshTokenExpirationMinutes;
+
+    public String encodeBase64SecretKey(String secretKey) {
         return Encoders.BASE64
                 .encode(secretKey.getBytes(StandardCharsets.UTF_8));
     }
@@ -47,13 +64,32 @@ public class JwtTokenizer {
                 .compact();
     }
 
-    public void verifySignature(String jws, String base64EncodedSeretKey) {
-        Key key = getKeyFromBase64EncodedKey(base64EncodedSeretKey);
+    // 검증 후, Claims를 반환하는 용도
+    public Jws<Claims> getClaims(String jws, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        Jws<Claims> claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jws);
+        return claims;
+    }
+
+    // 단순히 검증만 하는 용도
+    public void verifySignature(String jws, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
 
         Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(jws);
+    }
+
+    public Date getTokenExpiration(int expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationMinutes);
+
+        return calendar.getTime();
     }
 
     private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
