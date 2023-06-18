ed9c7749ce221886dc2132a890076a58c6fa4edc
Mason3144 <lts890303@gmail.com>
2023-06-18 23:14:55 +0900

    [feat] Question Entity와 Dto 생성 (no space) (#5, #7, #,8 #16, #63)

--- server/src/main/java/synergy_overflow/question/entity/Question.java
+++ server/src/main/java/synergy_overflow/question/entity/Question.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import synergy_overflow.helper.auditable.Auditable;
+import synergy_overflow.question.temporaries.temporaryEntities.AnswerEntity;
+import synergy_overflow.question.temporaries.temporaryEntities.MemberEntity;
 
 import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -16,4 +20,24 @@ public class Question extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long questionId;
+    @Column(nullable = false, length = 50)
+    private String title;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+    @Column(nullable = false,columnDefinition = "boolean default false")
+    private boolean adopted;
+    @Column(nullable = false)
+    private int views;
+
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity writer;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<AnswerEntity> answers = new LinkedList<>();
+
+    public void setAnswers(AnswerEntity answer){
+        this.answers.add(answer);
+        if(answer.getQuestion()!=this) answer.setQuestion(this);
+    }
 }
