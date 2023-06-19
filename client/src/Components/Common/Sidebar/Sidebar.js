b653da6e2d9d7b254a476d2f0e112b1d5b93c6d3
hjthebunny <monicahyunji@gmail.com>
2023-06-19 14:29:41 +0900

    [refactor] useNavigate 리팩토링

--- client/src/Components/Common/Sidebar/Sidebar.js
+++ client/src/Components/Common/Sidebar/Sidebar.js
@@ -1,56 +1,43 @@
 import { useState } from 'react';
-// import { Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import * as S from './Sidebar.styled';
 import { ReactComponent as Earth } from '../../../icons/earth.svg';
 
 //로그인 상태에 따라 home 클릭 시 페이지 이동을 달리하기 때문에 로그인 상태를 props로 전달받아야 합니다.  ex{isLoggedIn}
 
 function Sidebar() {
+  // const navigate = useNavigate();
   const [active, setActive] = useState('');
   const handleButtonClick = (name) => {
     setActive(name);
+  };
+  const goHome = () => {
+    //   if (로그인상태) {
+    //     navigate('/questions');
+    //   } else {
+    //     navigate('/home');
+    //   }
+    handleButtonClick('Home');
+  };
+  const goQuestions = () => {
+    //   navigate('/questions');
+    handleButtonClick('Questions');
   };
   return (
     <S.SidebarContainer>
       {/*로그인 상태 props를 전달받고
       페이지가 생성되었을 때,
       아래의 S.Home 컴포넌트를 삭제한 뒤 주석을 제거합니다.*/}
-      <S.Home
-        active={active === 'Home'}
-        onClick={() => handleButtonClick('Home')}
-      >
+      <S.Home active={active === 'Home'} onClick={goHome}>
         Home
       </S.Home>
-      {/* {isLoggedIn ? (
-        <Link to="/questions">
-          <S.Home
-            active={active === 'Home'}
-            onClick={() => handleButtonClick('Home')}
-          >
-            Home
-          </S.Home>
-        </Link>
-      ) : (
-        <Link to="/home">
-          <S.Home
-            active={active === 'Home'}
-            onClick={() => handleButtonClick('Home')}
-          >
-            Home
-          </S.Home>
-        </Link>
-      )} */}
+
       <S.Public>PUBLIC</S.Public>
       <S.Groups>
-        {/* <Link to="/questions/?page=1&size=20"> */}
-        <S.Questions
-          active={active === 'Questions'}
-          onClick={() => handleButtonClick('Questions')}
-        >
+        <S.Questions active={active === 'Questions'} onClick={goQuestions}>
           <Earth />
           Questions
         </S.Questions>
-        {/* </Link> */}
         <S.Tags>
           <S.Empty></S.Empty>
           Tags
