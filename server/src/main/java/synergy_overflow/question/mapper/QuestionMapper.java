8b0f918e3464fae7edfe44beef4f8975a24ba316
Mason3144 <lts890303@gmail.com>
2023-06-19 14:51:56 +0900

    [feat] 임시 dto와 entity를 위한 임시 맵핑 (#5, #7, #8, #16, #63)

--- server/src/main/java/synergy_overflow/question/mapper/QuestionMapper.java
+++ server/src/main/java/synergy_overflow/question/mapper/QuestionMapper.java
@@ -5,8 +5,11 @@ import org.mapstruct.ReportingPolicy;
 import synergy_overflow.question.dto.MultiResponseDto;
 import synergy_overflow.question.dto.QuestionDto;
 import synergy_overflow.question.entity.Question;
+import synergy_overflow.question.temporaries.temporaryDtos.AnswerDto;
+import synergy_overflow.question.temporaries.temporaryDtos.CommentsDto;
 import synergy_overflow.question.temporaries.temporaryDtos.WriterDto;
 import synergy_overflow.question.temporaries.temporaryEntities.AnswerEntity;
+import synergy_overflow.question.temporaries.temporaryEntities.CommentEntity;
 import synergy_overflow.question.temporaries.temporaryEntities.MemberEntity;
 
 import java.util.List;
@@ -16,8 +19,13 @@ public interface QuestionMapper {
     Question questionDtoPostToQuestion(QuestionDto.Post questionDtoPost);
     Question questionDtoPatchToQuestion(QuestionDto.Patch questionDtoPost);
     QuestionDto.Response questionToQuestionDtoResponse(Question question);
-    WriterDto.Response memberToWriterDtoResponse(MemberEntity member);
     int answersToAnswerNumber(List<AnswerEntity> answers);
     MultiResponseDto.MultiQuestionsResponse questionToMultiResponseDto(Question question);
     List<MultiResponseDto.MultiQuestionsResponse> questionsToMultiResponseDtos(List<Question> questions);
+
+    // 임시 dto 및 entity를 위한 임시 맵핑
+    WriterDto.Response memberToWriterDtoResponse(MemberEntity member);
+    AnswerDto.Response answerToAnswerDtoResponse(AnswerEntity answer);
+    CommentsDto.Response commentToCommentsDtoResponse(CommentEntity comment);
+
 }
