780744a902d56d6b89a66cc54e3983b87bdea5ac
isu-nice <isu010614@naver.com>
2023-06-19 21:03:31 +0900

    [refactor] Json 유틸 사용 (#13)

--- server/src/main/java/synergy_overflow/auth/utils/ErrorResponder.java
+++ server/src/main/java/synergy_overflow/auth/utils/ErrorResponder.java
@@ -11,10 +11,9 @@ import java.io.IOException;
 // ErrorResponse를 출력 스트림으로 생성하는 클래스
 public class ErrorResponder {
     public static void sendErrorResponse(HttpServletResponse response, HttpStatus status) throws IOException {
-        Gson gson = new Gson();
         ErrorResponse errorResponse = ErrorResponse.of(status);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(status.value());
-        response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
+        response.getWriter().write(JsonUtil.toJson(errorResponse, ErrorResponse.class));
     }
 }
