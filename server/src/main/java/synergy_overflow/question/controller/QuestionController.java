687bbcfd3cc2bdddbb4f6cb4e0ccdf49d6e6394b
Mason3144 <lts890303@gmail.com>
2023-06-19 13:40:15 +0900

    [feat] Question Controller 1차 완성(#5, #7, #8, #16, #63)

--- server/src/main/java/synergy_overflow/question/controller/QuestionController.java
+++ server/src/main/java/synergy_overflow/question/controller/QuestionController.java
@@ -1,39 +1,69 @@
 package synergy_overflow.question.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import synergy_overflow.question.dto.MultiResponseDto;
 import synergy_overflow.question.dto.QuestionDto;
+import synergy_overflow.question.dto.QuestionVo;
+import synergy_overflow.question.entity.Question;
+import synergy_overflow.question.mapper.QuestionMapper;
+import synergy_overflow.question.sevice.QuestionService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
 @Validated
 public class QuestionController {
-    @PostMapping("/ask")
-    public ResponseEntity postQuestion(){
-        return null;
+    private final QuestionService service;
+    private final QuestionMapper mapper;
+
+    public QuestionController(QuestionService service, QuestionMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
     }
+
+    @PostMapping("/ask")
+    public ResponseEntity postQuestion(@RequestBody @Valid QuestionDto.Post requestBody){
+        Question createdQuestion = service.createQuestion(mapper.questionDtoPostToQuestion(requestBody));
+
+        return new ResponseEntity<>(mapper.questionToQuestionDtoResponse(createdQuestion), HttpStatus.CREATED);
+    }
+
     @GetMapping("/{question-id}")
-    public ResponseEntity getQuestion(){
-        return null;
+    public ResponseEntity getQuestion(@PathVariable("question-id") @Positive long questionId){
+        Question foundQuestion = service.getQuestion(questionId);
+
+        return new ResponseEntity<>(mapper.questionToQuestionDtoResponse(foundQuestion), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity getQuestions(){
-        return null;
+    public ResponseEntity getQuestions(@RequestParam @Positive int page,
+                                       @RequestParam @Positive int size,
+                                       @RequestParam QuestionVo.Filter filter){
+        Page<Question> pageQuestions = service.getQuestions(page-1, size, filter);
+        List<Question> questions = pageQuestions.getContent();
+
+        return new ResponseEntity<>(new MultiResponseDto(mapper.questionsToMultiResponseDtos(questions),pageQuestions),
+                                    HttpStatus.OK);
     }
 
     @PatchMapping("/{questions-id}/edit")
-    public ResponseEntity patchQuestion(){
-        return null;
+    public ResponseEntity patchQuestion(@RequestBody QuestionDto.Patch requestBody){
+        Question editQuestion = service.editQuestion(mapper.questionDtoPatchToQuestion(requestBody));
+
+        return new ResponseEntity<>(mapper.questionToQuestionDtoResponse(editQuestion), HttpStatus.OK);
     }
 
     @DeleteMapping("/{question-id}")
-    public ResponseEntity deleteQuestion(){
-        return null;
+    public ResponseEntity deleteQuestion(@PathVariable("question-id") @Positive long questionId){
+        service.removeQuestion(questionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- server/src/main/java/synergy_overflow/question/sevice/QuestionService.java
+++ server/src/main/java/synergy_overflow/question/sevice/QuestionService.java
@@ -1,7 +1,9 @@
 package synergy_overflow.question.sevice;
 
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import synergy_overflow.question.dto.QuestionVo;
 import synergy_overflow.question.entity.Question;
 
 import javax.transaction.Transactional;
@@ -9,23 +11,24 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class QuestionService {
-    public Question createQuestion(){
+    public Question createQuestion(Question question){
+
         return null;
     }
 
-    public Question getQuestion(){
+    public Question getQuestion(long questionId){
         return null;
     }
 
-    public Question getQuestions(){
+    public Page<Question> getQuestions(int page, int size, QuestionVo.Filter filter){
         return null;
     }
 
-    public Question editQuestion(){
+    public Question editQuestion(Question question){
         return null;
     }
 
-    public Question removeQuestion(){
+    public Question removeQuestion(long questionId){
         return null;
     }
 }
