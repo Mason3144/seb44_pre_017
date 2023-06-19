9d7daa3aa18573ee4ff62761e5446ce3b1aa3156
isu-nice <isu010614@naver.com>
2023-06-19 16:37:10 +0900

    [refactor] 컨트롤러 전체 수정 (#13)

--- server/src/main/java/synergy_overflow/member/controller/MemberController.java
+++ server/src/main/java/synergy_overflow/member/controller/MemberController.java
@@ -2,66 +2,72 @@ package synergy_overflow.member.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import synergy_overflow.member.dto.MemberDto;
+import synergy_overflow.member.entity.Member;
 import synergy_overflow.member.mapper.MemberMapper;
 import synergy_overflow.member.service.MemberService;
-import synergy_overflow.member.entity.Member;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/members")
+@Validated
 public class MemberController {
     private final MemberService memberService;
     private final MemberMapper mapper;
-
 
     public MemberController(MemberService memberService, MemberMapper mapper) {
         this.memberService = memberService;
         this.mapper = mapper;
     }
 
-    // 로그인
-    @PostMapping("/auth/login")
-    public String loginForm(){
+    // 회원 등록
+    @PostMapping
+    public ResponseEntity postMember(@RequestBody @Valid MemberDto.Post requestBody) {
+        Member member = mapper.memberPostToMember(requestBody);
+        Member createdMember = memberService.createMember(member);
 
-        return "login";
+        MemberDto.Response response = mapper.memberToMemberResponse(createdMember);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    // 회원 가입
-    @PostMapping("/members")
-    public ResponseEntity createAccountForm(@RequestBody @Valid MemberDto.Post memberDTO){
-        Member response = memberService.createMember(mapper.memberPostDtoToMember(memberDTO));
-        return new ResponseEntity <>(response,HttpStatus.CREATED);
+    // 회원 정보 수정
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(
+            @PathVariable("member-id") @Positive long memberId,
+            @Valid @RequestBody MemberDto.Patch requestBody
+    ) {
+        requestBody.setMemberId(memberId);
+
+        Member member = mapper.memberPatchToMember(requestBody);
+        Member updatedMember = memberService.updateMember(member);
+
+        MemberDto.Response response = mapper.memberToMemberResponse(updatedMember);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 회원 조회
-    @GetMapping("/members/{member-id}")
-    public ResponseEntity findMember(@PathVariable("member-id") long memberid){
-        Member response = memberService.findMember(memberid);
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
 
-        return new ResponseEntity <>(mapper.MemberToGetMemberResponse(response) ,HttpStatus.OK);
-    }
+        Member member = memberService.findMember(memberId);
+        MemberDto.Response response = mapper.memberToMemberResponse(member);
 
-    // 회원 수정
-    @PatchMapping("/members/{member-id}")
-    public ResponseEntity patchMember(@PathVariable("member-id") long memberid,
-                                      @RequestBody @Valid MemberDto.Patch patchDto){
-
-        patchDto.setMemberId(memberid);
-        Member response =memberService.updateMember(mapper.memberPatchDtoToMember(patchDto));
-
-        return new ResponseEntity<>(mapper.MemberToGetMemberResponse(response),HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 회원 삭제
-    @DeleteMapping("/members/{member-id}")
-    public ResponseEntity deleteMember(@PathVariable("member-id") long memberid){
-        memberService.deleteMember(memberid);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
 
-        // 회원 정보만 삭제하면 답변 댓글 질문 데이터도 같이 삭제 ??
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
