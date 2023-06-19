e587b16f80c2b7cbf7299ad89926f47d5d7b5b4d
Dongwoo Joo <wnehddn1111@gmail.com>
2023-06-19 14:28:37 +0900

    [feat] 페이지 라우터 설정(#66)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,59 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<div>HomePage 홈 팀원소개 </div>} />
+          <Route path="/members" element={<div>SignupPage 회원가입</div>} />
+          <Route path="/members/welcome" element={<div>AfterSignupPage 회원가입 완료</div>} />
+          <Route path="/" element={<div>LoginPage 로그인 페이지</div>} />
+          <Route
+            path="/questions"
+            element={<div>MainPage Top Questions 페이지</div>}
+          />
+          <Route
+            path="/questions/?page=1&size=20"
+            element={<div>QuestionsPage All Questions</div>}
+          />
+          <Route
+            path="/questions/ask"
+            element={<div>QuestionCreatePage 질문 생성 페이지</div>}
+          />
+          <Route
+            path="/questions/{question-id}"
+            element={<div>QuestionDetailPage 질문 상세 페이지</div>}
+          />
+          <Route
+            path="/questions/{question-id}/update"
+            element={<div>QuestionUpdatePage 질문 수정 페이지</div>}
+          />
+          <Route
+            path="/questions/{question-id}/answers"
+            element={<div>AnswerUpdatePage 질문 수정 페이지</div>}
+          />
+          <Route
+            path="/members/{member-id}"
+            element={<div>MyPage 회원정보 수정</div>}
+          />
+          <Route
+            path="/members/{member-id}/delete"
+            element={<div>ProfileDelete 회원정보 삭제</div>}
+          />
+          <Route
+            path="/members/{member-id}/yourlogin"
+            element={<div>ProfileYourlogins My Logins</div>}
+          />
+          <Route
+            path="/members/{member-id}/yourlogin/change-password"
+            element={<div>MyPage 비밀번호 변경</div>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
