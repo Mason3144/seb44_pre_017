a94f0cf5a7e709dbb83188680c3f65ed76de1990
isu-nice <isu010614@naver.com>
2023-06-19 16:37:46 +0900

    [refactor] DTO 전체 수정 (#13)

--- server/src/main/java/synergy_overflow/member/dto/MemberDto.java
+++ server/src/main/java/synergy_overflow/member/dto/MemberDto.java
@@ -1,8 +1,8 @@
 package synergy_overflow.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -10,10 +10,8 @@ import javax.validation.constraints.Pattern;
 
 
 public class MemberDto {
-
-    // 회원가입 request Dto
     @Getter
-    @Setter
+    @AllArgsConstructor
     public static class Post {
         @NotBlank(message = "이메일을 입력해 주세요.")
         @Email(message = "이메일 형식으로 작성해주세요.")
@@ -27,32 +25,11 @@ public class MemberDto {
         @NotBlank(message = "닉네임을 입력해 주세요.")
         private String nickname;
     }
-    // 로그인 request Dto
+
     @Getter
-    @Setter
-    public static class LoginRequest{
-
-        @NotBlank(message = "이메일을 입력해 주세요.")
-        @Email(message = "이메일 형식으로 작성해주세요.")
-        private String email;
-
-        @NotBlank(message = "비밀번호를 입력해 주세요")
-        @Pattern(regexp = "(?=.*[A-Za-z])(?=.*[0-9])(?=.*\\W).{6,20}",
-                message = "비밀번호는 영문 대 소문자,숫자,특수문자를 포함하여 6자 이상 작성해 주세요")
-        private String password;
-    }
-//// 토큰 Response dto
-//    @RequiredArgsConstructor
-//    @Getter
-//    public static class LoginResponse{
-//        private final String acessToken;
-//    }
-
-    // 회원 수정 request Dto
-    @Getter
-    @Setter
-    public static class Patch{
-        private Long memberId;
+    @AllArgsConstructor
+    public static class Patch {
+        private long memberId;
 
         @NotBlank(message = "비밀번호를 입력해 주세요")
         @Pattern(regexp = "(?=.*[A-Za-z])(?=.*[0-9])(?=.*\\W).{6,20}",
@@ -61,17 +38,16 @@ public class MemberDto {
 
         @NotBlank(message = "닉네임을 입력해 주세요.")
         private String nickname;
+
+        public void setMemberId(long memberId) {
+            this.memberId = memberId;
+        }
     }
 
-    // Response Dto
     @Getter
-    @Setter
-    public static class Response{
-        private Long memberId;
-
-        private String email;
-
+    @AllArgsConstructor
+    public static class Response {
+        private long memberId;
         private String nickname;
-
     }
 }
