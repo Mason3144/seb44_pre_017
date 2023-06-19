fff4874568944894cdcfef1f56dcac4d7e734e4e
Mason3144 <lts890303@gmail.com>
2023-06-19 13:40:15 +0900

    [feat] Question Controller 1차 완성(#5, #7, #8, #16, #63)

--- server/src/main/java/synergy_overflow/question/mapper/QuestionMapper.java
+++ server/src/main/java/synergy_overflow/question/mapper/QuestionMapper.java
@@ -2,7 +2,22 @@ package synergy_overflow.question.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import synergy_overflow.question.dto.MultiResponseDto;
+import synergy_overflow.question.dto.QuestionDto;
+import synergy_overflow.question.entity.Question;
+import synergy_overflow.question.temporaries.temporaryDtos.WriterDto;
+import synergy_overflow.question.temporaries.temporaryEntities.AnswerEntity;
+import synergy_overflow.question.temporaries.temporaryEntities.MemberEntity;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface QuestionMapper {
+    Question questionDtoPostToQuestion(QuestionDto.Post questionDtoPost);
+    Question questionDtoPatchToQuestion(QuestionDto.Patch questionDtoPost);
+    QuestionDto.Response questionToQuestionDtoResponse(Question question);
+    WriterDto.Response memberToWriterDtoResponse(MemberEntity member);
+    int answersToAnswerNumber(List<AnswerEntity> answers);
+    MultiResponseDto.MultiQuestionsResponse questionToMultiResponseDto(Question question);
+    List<MultiResponseDto.MultiQuestionsResponse> questionsToMultiResponseDtos(List<Question> questions);
 }
