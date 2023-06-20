779d96b9daedbb9f88a80a085e52562fa95ffe06
Dongwoo Joo <wnehddn1111@gmail.com>
2023-06-20 21:31:25 +0900

    [feat] 사이드바 버튼 상태 구현중(#33)

---
+++ client/src/Components/Profile/ProfileSidebar.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react';
+// 1. Button 클릭 시, 클릭된 버튼만 주황색 컬러로 포커스 on, 나머지 버튼은 주황색 컬러로 포커스 false
+// 2. 클릭 시, 페이지 navigate
+import * as S from './ProfileSidebar.styled';
+
+const ProfileSidebar = () => {
+  //   const [selectedButton, setSelectedButton] = useState(null);
+
+  //   const handleButtonClick = (buttonName) => {
+  //     setSelectedButton(buttonName);
+  //   };
+  return (
+    <div>
+      <S.SidebarWrapper>
+        <S.Title>PERSONAL INFORMATION</S.Title>
+        <S.Button>Edit profile</S.Button>
+        <S.Button>Delete profile</S.Button>
+        <S.Title>Access</S.Title>
+        <S.Button>Your logins</S.Button>
+      </S.SidebarWrapper>
+    </div>
+  );
+};
+
+export default ProfileSidebar;
---
+++ client/src/Components/Profile/ProfileSidebar.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const SidebarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 224px;
+  height: 524px;
+  margin: 20px;
+`;
+
+export const PersonalInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  box-sizing: border-box;
+`;
+
+export const Access = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.div`
+  box-sizing: border-box;
+  border: 1px solid white;
+  border-radius: 20px;
+  width: 80px;
+  height: 35px;
+  color: white;
+  text-align: center;
+  padding-top: 7px;
+  background-color: #e5883e;
+  color: black;
+  font-size: 14px;
+`;
