c1722bc9d6fbc28156310d4dc251d1b745dee3cb
hjthebunny <monicahyunji@gmail.com>
2023-06-22 12:20:12 +0900

    [feat] homepage 구현

--- client/src/Pages/Common/HomePage/HomePage.styled.js
+++ client/src/Pages/Common/HomePage/HomePage.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div``;
+export const HomeContainer = styled.div``;
 
 export const GreetingText = styled.h1`
   display: flex;
@@ -17,7 +17,7 @@ export const GreetingText = styled.h1`
 `;
 
 export const GreetingContainer = styled.div`
-  height: 500px;
+  height: 700px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -26,18 +26,83 @@ export const GreetingContainer = styled.div`
 `;
 export const Team = styled.h1`
   height: 500px;
-  text-align: center;
-  padding-top: 50px;
-  font-size: 40px;
   background-color: #f8f9f9;
   letter-spacing: 20px;
 `;
-
+export const TeamTitle = styled.h1`
+  text-align: center;
+  font-size: 40px;
+  padding-top: 30px;
+  letter-spacing: 20px;
+  color: #3b3f43;
+`;
+export const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 15px;
+  gap: 30px;
+`;
+export const FE = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 100px;
+`;
+export const BE = styled(FE)``;
+export const MemberContainer = styled.div`
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+export const MemberImg = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+`;
+export const MemberName = styled.div`
+  font-size: 13px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  color: #333;
+`;
 export const Stack = styled.div`
   height: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #3b3f43;
-  color: #f8f9f9;
+  color: #000000;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+export const Front = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  align-items: center;
+  background-color: #fff;
+  width: 50%;
+  height: 40%;
+  padding-right: 14px;
+  border-radius: 30px;
+  box-shadow: 5px 5px 5px black;
+`;
+export const Back = styled(Front)`
+  gap: 10px;
+`;
+export const Frame = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 5px;
+`;
+
+export const Logo = styled.img`
+  height: 100px;
+  width: 100px;
 `;
