fa9f68e7fe9c2febdc4b82a0028df2221ecf918e
isu-nice <isu010614@naver.com>
2023-06-22 11:15:24 +0900

    [chore] 코드 수정

--- server/src/test/java/synergy_overflow/helper/question/QuestionStubData.java
+++ server/src/test/java/synergy_overflow/helper/question/QuestionStubData.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import synergy_overflow.member.dto.MemberDto;
-import synergy_overflow.member.entity.Member;
 import synergy_overflow.question.dto.MultiResponseDto;
 import synergy_overflow.question.dto.QuestionDto;
 import synergy_overflow.question.entity.Question;
